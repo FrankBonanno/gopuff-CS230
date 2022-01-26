073a8b76a5a48c18ddec7bc669200351eef2477b
Frank Bonanno <fb0023@mix.wvu.edu>
2022-01-26 01:46:09 -0500

    Top nav basic elements created. TODO: dropdown for languages

---
+++ src/app/NavBar/topnavbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'gp-topnavbar',
+    templateUrl: 'topnavbar.component.html',
+    styleUrls: ['topnavbar.component.css']
+})
+
+export class TopNavBarComponent {
+
+}
