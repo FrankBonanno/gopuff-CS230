3501c71ffdb57ed35c9c587a3ef2b0002055fc6e
Frank Bonanno <fb0023@mix.wvu.edu>
2022-01-27 00:00:05 -0500

    completed top and bottom nav bars

---
+++ src/app/NavBar/bottomnavbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'gp-bottomnavbar',
+    templateUrl: 'bottomnavbar.component.html',
+    styleUrls: ['bottomnavbar.component.css']
+})
+
+export class BottomNavBarComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BottomNavBarComponent } from './NavBar/bottomnavbar.component';
 import { TopNavBarComponent } from './NavBar/topnavbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavBarComponent
+    TopNavBarComponent,
+    BottomNavBarComponent
   ],
   imports: [
     BrowserModule
